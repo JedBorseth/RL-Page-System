4a37e1f22f270e79f93a260ed101852a9853da81
JedBorseth <jedborseth@gmail.com>
2025-03-08 15:09:14 -0800

    fix layout type error

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,12 @@
 import { auth } from "~/server/auth";
 import { redirect } from "next/navigation";
+import { type ReactNode } from "react";
 
-export const Layout = async ({ children }: { children: React.ReactNode }) => {
+export default async function Layout({ children }: { children: ReactNode }) {
   const session = await auth();
   if (!session) {
     redirect("/");
     return null;
   }
   return <main className="">{children}</main>;
-};
-export default Layout;
+}
